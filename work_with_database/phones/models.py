9f8b0255bf9072cb1b51716c1fa28ff2ed16aef0
Godz Dmitriy <woke@mail.ru>
2023-06-06 17:39:21 +0300

    ...

--- work_with_database/phones/models.py
+++ work_with_database/phones/models.py
@@ -6,8 +6,7 @@ class Phone(models.Model):
     id = models.CharField(max_length=50, primary_key=True)
     name = models.CharField(max_length=50, null=False)
     price = models.CharField(max_length=50)
-    image = models.CharField(max_length=50)
+    image = models.CharField(max_length=300)
     release_date = models.CharField(max_length=50)
     lte_exists = models.CharField(max_length=50)
     slug = models.SlugField(max_length=50)
-    pass
