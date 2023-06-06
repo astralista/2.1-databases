6a64d7864b31bb6d1baea308cef903ae5eb81445
Godz Dmitriy <woke@mail.ru>
2023-06-06 16:14:55 +0300

    start work

--- work_with_database/phones/models.py
+++ work_with_database/phones/models.py
@@ -3,4 +3,11 @@ from django.db import models
 
 class Phone(models.Model):
     # TODO: Добавьте требуемые поля
+    id = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, null=False)
+    price = models.CharField(max_length=50)
+    image = models.CharField(max_length=50)
+    release_date = models.CharField(max_length=50)
+    lte_exists = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50)
     pass
